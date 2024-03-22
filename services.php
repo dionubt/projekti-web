66f46a3de59ef79260a1906d68c6cd1ac2b186ff
dionubt <153142012+dionubt@users.noreply.github.com>
2024-03-22 15:23:53 +0100

    Add files via upload

---
+++ services.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dopebox</title>
+    <link rel="stylesheet" href="styles.css">
+    <style>
+     
+    </style>
+</head>
+<body>
+    
+    
+     
+    
+      <section id="product" class="full-page">
+        <div class="container">
+            <h2>Music Albums</h2><br><br>
+            <div class="album-container">
+               
+                <div class="album">
+                    <img src="Jack_Harlow_-_Jackman.png" alt="Album 1" width="300" height="300">
+                    <h3>Jack Harlow</h3>
+                    <p>Jackman (2023)</p>
+                    <p>Price: $19.99</p>
+                    <button class="buy-button" data-album="Album 1" data-price="10.99">Buy Now</button>
+                </div>
+                <div class="album">
+                    <img src="Portrait-The-Weeknd.jpg" alt="Album 1" width="300" height="300">
+                    <h3>The Weekend </h3>
+                    <p>Starboy (2016)</p>
+                    <p>Price: $10.99</p>
+                    <button class="buy-button" data-album="Album 1" data-price="10.99">Buy Now</button>
+                </div>
+                <div class="album">
+                    <img src="df7eeb83a07162b45ac2e882cac055de9411054a_cover.avif" alt="Album 1" width="300" height="300">
+                    <h3>Nirvana</h3>
+                    <p>Nevermind (1991)</p>
+                    <p>Price: $12.99</p>
+                    <button class="buy-button" data-album="Album 1" data-price="10.99">Buy Now</button>
+                </div>
+                <div class="album">
+                    <img src="Encore_(Eminem_album)_coverart.jpg" alt="Album 1" width="300" height="300">
+                    <h3>Eminem</h3>
+                    <p>Encore (2004)</p>
+                    <p>Price: $10.99</p>
+                    <button class="buy-button" data-album="Album 1" data-price="10.99">Buy Now</button>
+                </div>
+                <div class="album">
+                    <img src="613eDDaw9uL._UF894,1000_QL80_.jpg" alt="Album 1" width="300" height="300">
+                    <h3>One Direction</h3>
+                    <p>Up all night (2011)</p>
+                    <p>Price: $9.99</p>
+                    <button class="buy-button" data-album="Album 1" data-price="10.99">Buy Now</button>
+                </div>
+                <div class="album">
+                    <img src="6f52cd9bdf564954c9df5a574db26cc4.webp" alt="Album 1" width="300" height="300">
+                    <h3>Shakira</h3>
+                    <p>Shakira (2014)</p>
+                    <p>Price: $14.99</p>
+                    <button class="buy-button" data-album="Album 1" data-price="10.99">Buy Now</button>
+                </div>
+    
+               
+    
+            </div>
+        </div>
+    </section>
+
+
+</body>
+</html>
