825fae5939ecd8a2ef715c792aad8747185d9958
dionubt <153142012+dionubt@users.noreply.github.com>
2024-03-24 11:22:27 +0100

    Add files via upload

--- database.php
+++ database.php
@@ -1,22 +1,3 @@
-
-
-
-<?php
-
-$servername = "localhost";
-$db = "Projekti";
-$username = "root";
-$password = "";
-
-try{
-    $conn = new PDO("mysql:host=$servername; dbname=$db",$username, $password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) );
-
-    echo "Super";
-
-}catch(PDOException $e){
-    echo "Lidhja deshtoi: " . $e->getMessage();
-}
-
-
+<?php 
 ?>
 
